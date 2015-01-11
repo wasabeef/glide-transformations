df3c1e59a27af66fce6199bb7b6f43110753bd75
furiya-daichi <furiya_daichi@cyberagent.co.jp>
2015-01-11 22:45:29 +0900

    add RoundedCourners

---
+++ transformations/src/main/java/jp/wasabeef/glide/transformations/RoundedCornersTransformation.java
@@ -0,0 +1,68 @@
+package jp.wasabeef.glide.transformations;
+
+/**
+ * Copyright (C) 2015 Wasabeef
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import com.bumptech.glide.load.Transformation;
+import com.bumptech.glide.load.engine.Resource;
+import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool;
+import com.bumptech.glide.load.resource.bitmap.BitmapResource;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.Shader;
+
+public class RoundedCornersTransformation implements Transformation<Bitmap> {
+
+    private BitmapPool mBitmapPool;
+
+    private int radius;
+    private int margin;
+
+    public RoundedCornersTransformation(BitmapPool pool, int radius, int margin) {
+        this.radius = radius;
+        this.margin = margin;
+        mBitmapPool = pool;
+    }
+
+    @Override
+    public Resource<Bitmap> transform(Resource<Bitmap> resource, int outWidth, int outHeight) {
+        Bitmap source = resource.get();
+
+        int width = source.getWidth();
+        int height = source.getHeight();
+
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(bitmap);
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setShader(new BitmapShader(source, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP));
+        canvas.drawRoundRect(new RectF(margin, margin, width - margin, height - margin),
+                radius, radius, paint);
+        source.recycle();
+
+        return BitmapResource.obtain(bitmap, mBitmapPool);
+    }
+
+    @Override
+    public String getId() {
+        return "RoundedTransformation(radius=" + radius + ", margin=" + margin + ")";
+    }
+}
