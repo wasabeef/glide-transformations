8609efa2570f6c404e00bf5e635d0d7584694177
furiya-daichi <furiya_daichi@cyberagent.co.jp>
2015-01-11 22:45:02 +0900

    add CropSquare

---
+++ transformations/src/main/java/jp/wasabeef/glide/transformations/CropSquareTransformation.java
@@ -0,0 +1,62 @@
+package jp.wasabeef.glide.transformations;
+
+/**
+ * Copyright (C) 2015 Wasabeef
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import com.bumptech.glide.load.Transformation;
+import com.bumptech.glide.load.engine.Resource;
+import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool;
+import com.bumptech.glide.load.resource.bitmap.BitmapResource;
+
+import android.graphics.Bitmap;
+
+public class CropSquareTransformation implements Transformation<Bitmap> {
+
+    private BitmapPool mBitmapPool;
+    private int mWidth;
+    private int mHeight;
+
+    public CropSquareTransformation(BitmapPool pool) {
+        this.mBitmapPool = pool;
+    }
+
+    public CropSquareTransformation(BitmapPool pool, int width, int height) {
+        mBitmapPool = pool;
+        mWidth = width;
+        mHeight = height;
+    }
+
+    @Override
+    public Resource<Bitmap> transform(Resource<Bitmap> resource, int outWidth, int outHeight) {
+        Bitmap source = resource.get();
+        int size = Math.min(source.getWidth(), source.getHeight());
+
+        int x = (source.getWidth() - size) / 2;
+        int y = (source.getHeight() - size) / 2;
+
+        Bitmap bitmap = Bitmap.createBitmap(source, x, y, size, size);
+        if (bitmap != source) {
+            source.recycle();
+        }
+
+        return BitmapResource.obtain(bitmap, mBitmapPool);
+    }
+
+    @Override
+    public String getId() {
+        return "CropSquareTransform(width=" + mWidth + ", height=" + mHeight + ")";
+    }
+}
