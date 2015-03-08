4c23e382069c420f64027ff6d92863880d5cee4e
wasabeef <dadadada.chop@gmail.com>
2015-03-09 00:08:53 +0900

    fix Transparency

--- transformations/src/main/java/jp/wasabeef/glide/transformations/CropCircleTransformation.java
+++ transformations/src/main/java/jp/wasabeef/glide/transformations/CropCircleTransformation.java
@@ -43,20 +43,20 @@ public class CropCircleTransformation implements Transformation<Bitmap> {
         int width = (source.getWidth() - size) / 2;
         int height = (source.getHeight() - size) / 2;
 
-        Bitmap.Config config =
-                source.getConfig() != null ? source.getConfig() : Bitmap.Config.ARGB_8888;
-        Bitmap bitmap = mBitmapPool.get(size, size, config);
+        Bitmap bitmap = mBitmapPool.get(size, size, Bitmap.Config.ARGB_8888);
         if (bitmap == null) {
-            bitmap = Bitmap.createBitmap(size, size, config);
+            bitmap = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888);
         }
 
         Canvas canvas = new Canvas(bitmap);
         Paint paint = new Paint();
         BitmapShader shader = new BitmapShader(source, BitmapShader.TileMode.CLAMP,
                 BitmapShader.TileMode.CLAMP);
-        Matrix matrix = new Matrix();
-        matrix.setTranslate(-width, -height);
-        shader.setLocalMatrix(matrix);
+        if (width != 0 || height != 0) {
+            Matrix matrix = new Matrix();
+            matrix.setTranslate(-width, -height);
+            shader.setLocalMatrix(matrix);
+        }
         paint.setShader(shader);
         paint.setAntiAlias(true);
 
