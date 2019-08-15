abb8ad14f3da481307c51b210fe750689e2d28d9
wasabeef <dadadada.chop@gmail.com>
2019-08-15 13:15:24 +0900

    refactor to be simply

--- transformations/src/main/java/jp/wasabeef/glide/transformations/CropCircleWithBorderTransformation.java
+++ transformations/src/main/java/jp/wasabeef/glide/transformations/CropCircleWithBorderTransformation.java
@@ -1,14 +1,30 @@
 package jp.wasabeef.glide.transformations;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+import androidx.annotation.ColorInt;
+import androidx.annotation.NonNull;
+
+import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool;
+import com.bumptech.glide.load.resource.bitmap.TransformationUtils;
+
+import java.security.MessageDigest;
+
+import jp.wasabeef.glide.transformations.internal.Utils;
+
 /**
- * Copyright (C) 2017 Wasabeef
- *
+ * Copyright (C) 2019 Wasabeef
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,79 +32,65 @@ package jp.wasabeef.glide.transformations;
  * limitations under the License.
  */
 
-public class CropCircleWithBorderTransformation implements Transformation<Bitmap> {
+public class CropCircleWithBorderTransformation extends BitmapTransformation {
 
-    private BitmapPool mBitmapPool;
-    private int borderWidth;
-    private int borderColor;
 
-    public CropCircleWithBorderTransformation(Context context, int borderWidth,
-                                              @ColorInt int borderColor) {
-        this(Glide.get(context).getBitmapPool());
-        this.borderWidth = borderWidth;
-        this.borderColor = borderColor;
-    }
+  private static final int VERSION = 1;
+  private static final String ID = "jp.wasabeef.glide.transformations.CropCircleWithBorderTransformation." + VERSION;
 
-    public CropCircleWithBorderTransformation(BitmapPool pool) {
-        this.mBitmapPool = pool;
-    }
+  private int borderSize;
+  private int borderColor;
 
-    @Override
-    public Resource<Bitmap> transform(Resource<Bitmap> resource, int outWidth, int outHeight) {
-        Bitmap source = resource.get();
-        int size = Math.min(source.getWidth(), source.getHeight());
 
-        int width = (source.getWidth() - size) / 2;
-        int height = (source.getHeight() - size) / 2;
+  public CropCircleWithBorderTransformation() {
+    this.borderSize = Utils.toDp(4);
+    this.borderColor = Color.BLACK;
+  }
 
-        Bitmap bitmap = mBitmapPool.get(size, size, Bitmap.Config.ARGB_8888);
-        if (bitmap == null) {
-            bitmap = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888);
-        }
+  public CropCircleWithBorderTransformation(int borderSize, @ColorInt int borderColor) {
+    this.borderSize = borderSize;
+    this.borderColor = borderColor;
+  }
 
-        Canvas canvas = new Canvas(bitmap);
-        Paint paint = new Paint();
-        BitmapShader shader =
-                new BitmapShader(source, BitmapShader.TileMode.CLAMP, BitmapShader.TileMode.CLAMP);
-        if (width != 0 || height != 0) {
-            // source isn't square, move viewport to center
-            Matrix matrix = new Matrix();
-            matrix.setTranslate(-width, -height);
-            shader.setLocalMatrix(matrix);
-        }
-        paint.setShader(shader);
-        paint.setAntiAlias(true);
+  @Override
+  protected Bitmap transform(@NonNull Context context, @NonNull BitmapPool pool,
+                             @NonNull Bitmap toTransform, int outWidth, int outHeight) {
 
-        float r = size / 2f;
-        canvas.drawCircle(r, r, r, paint);
+    Bitmap bitmap = TransformationUtils.circleCrop(pool, toTransform, outWidth, outHeight);
 
-        // add border
-        int dstBitmapWidth = bitmap.getWidth() + borderWidth * 2;
-        Bitmap borderedBitmap =
-                Bitmap.createBitmap(dstBitmapWidth, dstBitmapWidth, Bitmap.Config.ARGB_8888);
-        Canvas canvas1 = new Canvas(borderedBitmap);
-        canvas1.drawBitmap(bitmap, borderWidth, borderWidth, null);
+    setCanvasBitmapDensity(toTransform, bitmap);
 
-        Paint paint1 = new Paint();
-        paint1.setColor(borderColor);
-        paint1.setStyle(Paint.Style.STROKE);
-        paint1.setStrokeWidth(borderWidth);
-        paint1.setAntiAlias(true);
+    Paint paint = new Paint();
+    paint.setColor(borderColor);
+    paint.setStyle(Paint.Style.STROKE);
+    paint.setStrokeWidth(borderSize);
+    paint.setAntiAlias(true);
 
-        canvas1.drawCircle(
-                canvas1.getWidth() / 2,
-                canvas1.getWidth() / 2,
-                canvas1.getWidth() / 2 - borderWidth / 2,
-                paint1
-        );
+    Canvas canvas = new Canvas(bitmap);
+    canvas.drawCircle(
+        outWidth / 2f,
+        outHeight / 2f,
+        Math.max(outWidth, outHeight) / 2f - borderSize / 2f,
+        paint
+    );
 
-        bitmap.recycle();
+    return bitmap;
+  }
 
-        return BitmapResource.obtain(borderedBitmap, mBitmapPool);
-    }
+  @Override
+  public void updateDiskCacheKey(@NonNull MessageDigest messageDigest) {
+    messageDigest.update((ID + borderSize + borderColor).getBytes(CHARSET));
+  }
 
-    @Override public String getId() {
-        return "CropCircleWithBorderTransformation(" +
-                "width=" + borderWidth + ", color=" + borderColor + ")";
-    }
+  @Override
+  public boolean equals(Object o) {
+    return o instanceof CropCircleWithBorderTransformation &&
+        ((CropCircleWithBorderTransformation) o).borderSize == borderSize &&
+        ((CropCircleWithBorderTransformation) o).borderColor == borderColor;
+  }
+
+  @Override
+  public int hashCode() {
+    return ID.hashCode() + borderSize * 100 + borderColor + 10;
+  }
 }
