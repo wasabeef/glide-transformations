681e027fd1fc1ebf138f9d3705b2327a80e9d43b
wasabeef <dadadada.chop@gmail.com>
2019-08-15 13:17:13 +0900

    add px to dp convert

--- transformations/src/main/java/jp/wasabeef/glide/transformations/internal/Utils.java
+++ transformations/src/main/java/jp/wasabeef/glide/transformations/internal/Utils.java
@@ -1,18 +1,19 @@
 package jp.wasabeef.glide.transformations.internal;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 
 /**
  * Copyright (C) 2019 Wasabeef
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -39,5 +40,9 @@ public final class Utils {
     }
 
     return drawable;
+  }
+
+  public static int toDp(int px) {
+    return px * (int) Resources.getSystem().getDisplayMetrics().density;
   }
 }
